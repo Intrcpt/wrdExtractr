b4ac50c50f4bca4dc8e4ded456fd25abe7d463a7
Intrcpt <98618504+Intrcpt@users.noreply.github.com>
2022-11-22 20:09:02 -0600

    Code Refactoring - moved the part of the code that fetches the HTML into a function and then changed the last line of the code to print the result of this function call instead of a variable

--- wrdExtractr.py
+++ wrdExtractr.py
@@ -2,12 +2,15 @@ import requests
 
 PAGE_URL = 'http://URL'
 
-resp = requests.get(PAGE_URL)
+# fetches the HTML into a function
+def get_html_of(url):
+    resp = requests.get(url)
 
-if resp.status_code != 200:
-    print(f'HTTP status code of {resp.status_code} returned, but 200 was expected. Exiting...')
-    exit(1)
+    if resp.status_code != 200:
+        print(f'HTTP status code of {resp.status_code} returned, but 200 was expected. Exiting...')
+        exit(1)
 
-html_str = resp.content.decode()
-print(html_str)
+    return resp.content.decode()
 
+# print the result of this function call  
+print(get_html_of(PAGE_URL))
