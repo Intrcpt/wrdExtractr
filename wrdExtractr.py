abd168753cbfe2b9c1ccfca79dd09c4c364406d1
Intrcpt <98618504+Intrcpt@users.noreply.github.com>
2022-11-22 21:21:06 -0600

    Added a For loop that counts the occurrences of words in memory.

--- wrdExtractr.py
+++ wrdExtractr.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 PAGE_URL = 'http://URL'
 
-# fetches the HTML into a function
+# fetches the HTML into a function and does a simple fail check for broken links
 def get_html_of(url):
     resp = requests.get(url)
 
@@ -20,3 +20,17 @@ soup = BeautifulSoup(html, 'html.parser')
 raw_text = soup.get_text()
 #  r'\w+' is telling Python to interpret the \w part of the string as two individual characters and not an escaped w.
 all_words = re.findall(r'\w+', raw_text)
+
+# word_count as an empty dictionary - a data structure of key/value pairs allowing the lookup of some value given some key.
+word_count = {}
+
+# Goes through each word in all_words and checks if it exists already.
+for word in all_words:
+# Sets the key (word) to a value of 1 if it does not exist.
+    if word not in word_count:
+        word_count[word] = 1
+        
+# Otherwise (else), we get the current value set for (word) and set the new value of word to the previous value plus one.        
+    else:
+        current_count = word_count.get(word)
+        word_count[word] = current_count + 1
