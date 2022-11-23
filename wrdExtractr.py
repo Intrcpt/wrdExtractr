180c97d6e180709fda5906fc4f9a47c9a28a95d3
Intrcpt <98618504+Intrcpt@users.noreply.github.com>
2022-11-22 19:42:15 -0600

    Added fail check that makes sure we do not try to work with broken links

--- wrdExtractr.py
+++ wrdExtractr.py
@@ -3,6 +3,11 @@ import requests
 PAGE_URL = 'http://URL'
 
 resp = requests.get(PAGE_URL)
+
+if resp.status_code != 200:
+    print(f'HTTP status code of {resp.status_code} returned, but 200 was expected. Exiting...')
+    exit(1)
+
 html_str = resp.content.decode()
 print(html_str)
 
